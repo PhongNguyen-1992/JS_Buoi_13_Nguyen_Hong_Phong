14bf31ec4560922ac84c64174edf2c5611afabc6
PhongNguyen-1992 <phongnguyenfpt1992@gmail.com>
2025-05-10 15:42:55 +0700

    final

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function getID(id) {
     return document.getElementById(id)
 }
+// Nút Bài 1
 function BTN1() {
     let totalNumber = getID("totalNumber").value * 1;
     let number1 = getID("number1").value * 1;
@@ -17,6 +18,72 @@ function BTN1() {
     getID("noti").innerHTML = content
 }
 
+// Nút Bài 2
+
+function Muc_1(kw, price) {
+    const rs = kw * price;
+    return rs;
+}
+function Muc_2(kw, price) {
+    const rs = (kw - 50) * price;
+    return rs;
+}
+function Muc_3(kw, price) {
+    const rs = (kw - 100) * price;
+    return rs;
+}
+function Muc_4(kw, price) {
+    const rs = (kw - 200) * price;
+    return rs;
+}
+function Muc_5(kw, price) {
+    const rs = (kw - 350) * price;
+    return rs;
+}
+
+function BTN2() {
+    const name_btn2 = getID("name_btn2").value;
+    const soDien = getID("soDien").value * 1;
+    result_2 = 0;
+    if (0 <= soDien && soDien <= 50) {
+        const Phs1 = Muc_1(soDien, 500)
+        result_2 = Phs1
+    } else if (50 < soDien && soDien <= 100) {
+        const Phs1 = Muc_1(50, 500);
+        const Phs2 = Muc_2(100, 650);
+        result_2 = Phs1 + Phs2
+
+    } else if (100 < soDien && soDien <= 200) {
+        const Phs1 = Muc_1(50, 500);
+        const Phs2 = Muc_2(100, 650);
+        const Phs3 = Muc_3(200, 500);
+        result_2 = Phs1 + Phs2 + Phs3
+
+    } else if (200 < soDien && soDien <= 350) {
+        const Phs1 = Muc_1(50, 500);
+        const Phs2 = Muc_2(100, 650);
+        const Phs3 = Muc_3(200, 850);
+        const Phs4 = Muc_4(350, 1100);
+
+        result_2 = Phs1 + Phs2 + Phs3 + Phs4
+
+    } else if (soDien > 350) {
+        const Phs1 = Muc_1(50, 500);
+        const Phs2 = Muc_2(100, 650);
+        const Phs3 = Muc_3(200, 850);
+        const Phs4 = Muc_4(350, 1100);
+        const Phs5 = Muc_5(soDien, 1300);
+
+        result_2 = Phs1 + Phs2 + Phs3 + Phs4 + Phs5
+
+    } else {
+        alert("Vui lòng nhập số điện đúng")
+        return;
+    }
+    content = `Tiền điện tháng: ${result_2.toLocaleString("vi-VN")} VNĐ`
+    getID("noti2").innerHTML = content
+}
+// Nút Bài 3
 function BTN3() {
     let name = getID("name").value;
     let totalPrice = getID("totalPrice").value * 1;
@@ -32,18 +99,43 @@ function BTN3() {
         result = chiu_thue * 0.1;
     } else if (120000000 < chiu_thue && chiu_thue <= 210000000) {
         result = chiu_thue * 0.15;
-    }else if (210000000 < chiu_thue && chiu_thue <= 384000000) {
+    } else if (210000000 < chiu_thue && chiu_thue <= 384000000) {
         result = chiu_thue * 0.2;
-    }else if (384000000 < chiu_thue && chiu_thue <= 624000000) {
+    } else if (384000000 < chiu_thue && chiu_thue <= 624000000) {
         result = chiu_thue * 0.25;
-    }else if (624000000 < chiu_thue && chiu_thue <= 960000000) {
+    } else if (624000000 < chiu_thue && chiu_thue <= 960000000) {
         result = chiu_thue * 0.3;
-    }else if (960000000 < chiu_thue) {
+    } else if (960000000 < chiu_thue) {
         result = chiu_thue * 0.35;
-    }else {
-       content = "Không phải đóng thuế"; 
+    } else {
+        content = "Không phải đóng thuế";
     }
 
     content = `Thuế phải đóng là: ${result.toLocaleString("vi-VN")} VNĐ`
     getID("noti3").innerHTML = content
 }
+// Nút Bài 4
+function BTN4() {
+    let name = getID("name").value;
+    let loaiKHG = getID("loaiKHG").value * 1;
+    let coBan = getID("coBan").value * 1;
+    let caoCap = getID("caoCap").value * 1;
+    let result_4 = 0;
+    let content_4 = "";
+    if (loaiKHG === 1) {
+        result_4 = 4.5 + 20.5 + (caoCap * 7.5)
+
+    } else if (loaiKHG === 2 && coBan > 10) {
+        result_4 = 15 + 75 + ((coBan - 10) * 5) + (caoCap * 50)
+
+    } else if (loaiKHG === 2 && coBan <= 10) {
+        result_4 = 15 + 75 + (caoCap * 50)
+
+    }
+
+
+
+    content = `Tiền thuê cáp của quý khách: ${result_4} $`
+    getID("noti4").innerHTML = content
+}
+
